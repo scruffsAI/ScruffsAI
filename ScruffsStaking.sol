6025a47201f6f44b0c9d5cd4080a9197dbe40235
scruffsAI <scruffscoin@gmail.com>
2025-01-03 04:13:37 -0800

    Create ScruffsStaking.sol

---
+++ ScruffsStaking.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/// @title ScruffsStaking
+/// @notice A staking contract to reward loyal Scruffs holders.
+contract ScruffsStaking {
+    mapping(address => uint256) public stakes;
+
+    function stake(uint256 amount) public {
+        // Staking logic here
+    }
+}
